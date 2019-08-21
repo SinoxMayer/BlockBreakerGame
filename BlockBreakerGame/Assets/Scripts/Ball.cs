b73055fa6dbde0a1052db6ab6f6e1d592d1bd9f3
SinoxMayer <arda_kaptan@hotmail.com>
2019-08-21 08:41:56 +0300

    Lenovo21
    21 08 2019

---
+++ BlockBreakerGame/Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{      // başka objeyi burdan bağladım
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+        //State
+    Vector2 paddleToBeBallVector;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBeBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+
+        }
+      
+        LaunchOnMouseClick();
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBeBallVector;
+        
+    }
+
+    
+}
---
+++ BlockBreakerGame/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float ScreenWidthUnits = 16f;
+    [SerializeField] float xMin = 1.0f, xMax = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition.x/Screen.width*ScreenWidthUnits);
+        float xPos = Mathf.Clamp( Input.mousePosition.x / Screen.width * ScreenWidthUnits ,xMin,xMax  );
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = xPos;
+        transform.position = paddlePos;
+    }
+}
